627aabc1b46035def795f04c96a3656825ff7028
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-04-26 09:51:57 +0200

    kk

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -16,6 +16,8 @@ const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 // How many rounds should bcrypt run the salt (default - 10 rounds)
 const saltRounds = 10;
 
+
+
 // POST /auth/signup  - Creates a new user in the database
 router.post("/signup", (req, res, next) => {
   const { email, password, name } = req.body;
@@ -106,7 +108,7 @@ router.post("/login", (req, res, next) => {
         const payload = { _id, email, name };
 
         // Create a JSON Web Token and sign it
-        const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+        const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {  
           algorithm: "HS256",
           expiresIn: "6h",
         });
