7ba28d1be4957273b2f1b17178b55809b4a4505b
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-04-27 09:04:37 +0200

    kk

--- config/index.js
+++ config/index.js
@@ -28,11 +28,18 @@ module.exports = (app) => {
     })
   );
 
-  app.options('*', (req, res) => {
-    res.set('Access-Control-Allow-Origin', FRONTEND_URL);
-    res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.status(204).end();
+  // Middleware to set CORS headers
+  app.use((req, res, next) => {
+    // Set CORS headers
+    res.header("Access-Control-Allow-Origin", FRONTEND_URL);
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+    );
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type");
+
+    // Continue to the next middleware or route handler
+    next();
   });
 
   // In development environment the app logs
