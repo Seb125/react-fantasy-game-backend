3278e1cde52492fba37161cefbdcf55b008603d4
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-04-27 08:55:25 +0200

    preflight cors

--- config/index.js
+++ config/index.js
@@ -28,6 +28,13 @@ module.exports = (app) => {
     })
   );
 
+  app.options('*', (req, res) => {
+    res.set('Access-Control-Allow-Origin', FRONTEND_URL);
+    res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.status(204).end();
+  });
+
   // In development environment the app logs
   app.use(logger("dev"));
 
